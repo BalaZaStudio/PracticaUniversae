439ec4a3871ee867daaab95aae4af2625fa5759a
ZaBaDeV <zabal_9@hotmail.com>
2024-04-10 14:01:52 +0200

    Inicio

---
+++ src/proyectouniversae/ProyectoUniversae.java
@@ -0,0 +1,17 @@
+package proyectouniversae;
+
+/**
+ * @author zabadev
+ */
+public class ProyectoUniversae {
+
+    public static void main(String[] args) {
+
+        PantallaPrincipal princ = new PantallaPrincipal();
+        princ.setVisible(true);
+        
+        
+        
+    }
+
+}
